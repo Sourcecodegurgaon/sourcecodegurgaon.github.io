04f2d08cd4aa31e9ce3705e50141b0dcdef65c46
Sourcecodegurgaon <58719488+Sourcecodegurgaon@users.noreply.github.com>
2019-12-17 00:26:13 +0530

    Add files via upload

--- styles-es5.js
+++ styles-es5.js
@@ -276,7 +276,7 @@
           \******************************/
         /*! no static exports found */
         /***/ (function (module, exports, __webpack_require__) {
-            module.exports = __webpack_require__(/*! C:\Users\13\Desktop\streethok\src\styles.css */ "./src/styles.css");
+            module.exports = __webpack_require__(/*! C:\Users\God\Desktop\street\src\styles.css */ "./src/styles.css");
             /***/ 
         })
     }, [[3, "runtime"]]]);
